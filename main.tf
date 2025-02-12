434460e47b237696bd7fa755c9948a43b6cf2ca8
Anku7721 <ankitak7721@gmail.com>
2025-02-12 16:33:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 # Private Subnet
 resource "aws_subnet" "pvt_subnet" {
   vpc_id            = data.aws_vpc.vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.20.0/24"
   map_public_ip_on_launch = false
   
   tags = {
